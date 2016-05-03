b6d30f8514acbfbd1ea87c502ad0ffb4eccc9cc8
lina1 <nali0908@gmail.com>
2016-05-03 23:55:08 +0800

    pandas series

--- numpy_/pandas_.py
+++ numpy_/pandas_.py
@@ -11,3 +11,21 @@ s = Series([100, "python", "go", "lua"])
 print s
 print s.values
 print s.index
+
+s2 = Series([100, "python", "scu", "lina"], index=["mark", "title", "university", "name"])
+print s2
+print s2.index
+print s2["name"]
+
+sd = {"python": 10000, "go": 8900, "lua": 7200}
+s3 = Series(sd)
+print s3
+
+s4 = Series(sd, index=["python", "go", "java"])
+print s4
+
+print pd.isnull(s4)
+print s4.isnull()
+
+s4.index = ["p1", "p2", "p3", "p4"]
+print s4
